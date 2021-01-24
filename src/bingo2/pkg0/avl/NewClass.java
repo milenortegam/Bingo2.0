5d00d947e913a9a2c8a94db6838b9f3f029920da
Hayleen Carrillo <haivcarr@espol.edu.ec>
2021-01-23 20:00:13 -0500

    Refactoring

---
+++ src/bingo2/pkg0/avl/NewClass.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import bingo2.pkg0.avl.TreeAVL;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Scanner;
+/**
+
+import static bingo2.pkg0.Bingo20.buscarNumeros;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class NewClass {
+    //DIFERENTE
+    /*
+    //que reciba i (max de numeros), color de cartones
+    System.out.println("Ahora la ronda de los cartones Azules");
+    //IGUAL
+    System.out.println("Ingrese números entre 0-20");
+    boolean cierto2=true;
+    int i2=1;
+    
+    while(cierto2){
+            
+            
+            if(i2<15){
+                System.out.println("Número ["+i2+"]:");
+                Integer numero= sc.nextInt(); 
+                if(numero<=20){
+                    //DIFERENTE
+                    String tmp=buscarNumeros(Azul, numero);
+                    //
+                    if(tmp!=null){
+                        System.out.println("El ganador de esta ronda es: "+tmp);
+                        cierto2=false;
+                    }
+
+                    i2++; 
+                }
+                else{
+                    System.out.println("El número que ingreso es mayor a 20 ingrese otro por favor");
+                     
+                }
+            }
+            
+            else if(i2==15){
+                System.out.println("Como no se ha encontrado ganador, vamos a la ronda extra");
+                System.out.println("Ingresa un número: ");
+                Integer numero= sc.nextInt(); 
+                if(numero<=20){
+                    String tmp=buscarNumeros(Azul, numero);
+                    if(tmp!=null){
+                        System.out.println("El ganador de esta ronda es: "+tmp);
+                        break;
+                    }
+                    
+                    System.out.println("Lo sentimos, no existe tabla ganadora en la ronda de los Azul");
+                    cierto2=false;
+                }
+                else{
+                    System.out.println("El número que ingreso es mayor a 20 ingrese otro por favor");
+                }
+                
+            }
+            else{
+                cierto2=false;
+            }  
+            }*/
+}
